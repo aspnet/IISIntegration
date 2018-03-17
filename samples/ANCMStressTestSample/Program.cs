e79156d3bb4b3f883fa3b589afb6b6ef57a8e8ce
Yanbing Shi <yashi@microsoft.com>
2018-03-16 18:14:59 -0700

    initial checkin

---
+++ samples/ANCMStressTestSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ANCMStressTestApp
+{
+    public class Program
+    {
+        public static IApplicationLifetime AppLifetime;
+        public static bool AppLifetimeStopping = false;
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .ConfigureLogging((_, factory) =>
+                {
+                    factory.AddConsole();
+                })
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            AppLifetime = (IApplicationLifetime)host.Services.GetService(typeof(IApplicationLifetime));
+            AppLifetime.ApplicationStopping.Register(
+                () => {
+                    AppLifetimeStopping = true;
+                }
+            );
+
+            host.Run();
+        }
+    }
+}
