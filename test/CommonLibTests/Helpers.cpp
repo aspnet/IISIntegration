282f80e18d938461aacb81abb7cbf7d19ebd0c22
Justin Kotalik <jukotali@microsoft.com>
2018-06-01 11:59:38 -0700

    Tests

--- test/CommonLibTests/Helpers.cpp
+++ test/CommonLibTests/Helpers.cpp
@@ -31,10 +31,18 @@ Helpers::ReadFileContent(std::wstring file)
 {
     std::wcout << file << std::endl;
 
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-
     std::fstream t(file);
     std::stringstream buffer;
     buffer << t.rdbuf();
-    return std::wstring(converter.from_bytes(buffer.str()));
+
+    int nChars = MultiByteToWideChar(CP_ACP,  0, buffer.str(), -1, NULL, 0);
+
+    LPWSTR pwzName = new WCHAR[nChars];
+    MultiByteToWideChar(CP_UTF8, 0, buffer.str(), -1, pwzName, nChars);
+
+    std::wstring retVal(pwzName);
+
+    delete pwzName;
+
+    return retVal;
 }
