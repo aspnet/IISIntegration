38c34b164fe1abcb2b64e0144c1595d88fbaf2bc
Justin Kotalik <jukotali@microsoft.com>
2018-08-02 17:27:16 -0700

    Some comments

--- src/AspNetCoreModuleV2/RequestHandlerLib/PipeWrapper.cpp
+++ src/AspNetCoreModuleV2/RequestHandlerLib/PipeWrapper.cpp
@@ -7,50 +7,48 @@
 #include "LoggingHelpers.h"
 
 PipeWrapper::PipeWrapper(FILE* outputStream, DWORD nHandle, HANDLE pipeHandle)
-    : outputStream(outputStream),
+    : stdStream(outputStream),
       nHandle(nHandle),
-      pipeHandle(pipeHandle)
+      writerHandle(pipeHandle)
 {
 }
 
-// Log if last error stuff in this class
-
-PipeWrapper::~PipeWrapper()
-{
-}
+PipeWrapper::~PipeWrapper() = default;
 
 HRESULT
 PipeWrapper::
 SetupRedirection()
 {
-    previousFileDescriptor = _dup(_fileno(outputStream));
+    // Duplicate the current stdstream to be used for restoring. 
+    previousFileDescriptor = _dup(_fileno(stdStream));
+
+    // in IIS, stdout and stderr are both invalid initially, so _dup will return -1
+    // Open a null file to restore back and save the previous file descriptor
     if (previousFileDescriptor == -1)
     {
         FILE* dummyFile;
-        freopen_s(&dummyFile, "nul", "w", outputStream);
-        previousFileDescriptor = _fileno(outputStream);
+        freopen_s(&dummyFile, "nul", "w", stdStream);
+        previousFileDescriptor = _fileno(stdStream);
     }
 
-    RETURN_LAST_ERROR_IF(!SetStdHandle(nHandle, pipeHandle));
+    // set the std handle to the  
+    RETURN_LAST_ERROR_IF(!SetStdHandle(nHandle, writerHandle));
 
-    redirectedFile = LoggingHelpers::ReReadStdFileNo(nHandle, outputStream);
+    // After setting the std handle, we need to set stdout/stderr to the current
+    // output/error handle.
+    redirectedFile = LoggingHelpers::ReReadStdFileNo(nHandle, stdStream);
 
     return S_OK;
 }
 
 HRESULT
-PipeWrapper::
-StopRedirection()
+PipeWrapper::StopRedirection() const
 {
-    fflush(outputStream);
-    if (previousFileDescriptor >= 0)
-    {
-        LOG_LAST_ERROR_IF(!SetStdHandle(nHandle, (HANDLE)_get_osfhandle(previousFileDescriptor)));
-    }
+    LOG_LAST_ERROR_IF(!SetStdHandle(nHandle, reinterpret_cast<HANDLE>(_get_osfhandle(previousFileDescriptor))));
 
-    fclose(redirectedFile);
-    LoggingHelpers::ReReadStdFileNo(nHandle, outputStream);
+    LOG_LAST_ERROR_IF(fclose(redirectedFile));
+
+    LoggingHelpers::ReReadStdFileNo(nHandle, stdStream);
 
     return S_OK;
 }
-
